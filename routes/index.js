58893f1159e46848581c685c949447e74638bd53
Silvija Prozinger <37271365+SilvijaProzinger@users.noreply.github.com>
2020-03-24 19:46:30 +0100

    changed home page

--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,14 @@ const express = require('express');
 const router = express.Router();
 const getResults = require("../index");
 
-// home page. 
-router.get('/', async function(req, res, next) {
-  const searchWord = 'frontend' //by default we'll search for frontend jobs
-  const result = await getResults(searchWord)
-  res.render('index', result);
+// home page
+router.get('/', function(req, res) {
+  res.render('home');
 });
 
 // search page
 router.get('/search', async function(req, res, next) {
   const searchWord = req.query.keyword //the search word now changes based on what the user typed in
-  console.log(searchWord)
   const result = await getResults(searchWord)
   res.render('index', result);
 });
